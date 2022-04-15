e89a4441a20d7488181e69978a943eeeecb13435
JuanLP <juan.luis.palacios.p@gmail.com>
2022-04-14 23:09:23 -0500

    Add date filter controls

--- src/views/Countries.js
+++ src/views/Countries.js
@@ -5,7 +5,9 @@ import { getCountries } from '../redux/countries/countries';
 
 export default function Countries() {
   const [date, setDate] = useState(new Date());
-  const countries = useSelector((state) => state.countries);
+  let temp;
+  // eslint-disable-next-line no-multi-assign
+  const { countries, total } = temp = useSelector((state) => state.countries);
   const dispatch = useDispatch();
   useEffect(
     () => {
@@ -13,21 +15,54 @@ export default function Countries() {
     },
     [date],
   );
-  const a = [
-    { date: new Date(date.setDate(date.getDate() - 2)), id: 0 },
-    { date: new Date(date.setDate(date.getDate() + 1)), id: 1 },
-    { date: new Date(date.setDate(date.getDate() + 1)), id: 2 },
-    { date: new Date(date.setDate(date.getDate() + 1)), id: 3 },
-    { date: new Date(date.setDate(date.getDate() + 1)), id: 4 },
-  ];
-  date.setDate(date.getDate() - 2);
+  const prevYear = new Date(new Date().setFullYear(date.getFullYear() - 1));
+  const nextYear = new Date(new Date().setFullYear(date.getFullYear() + 1));
+  const prevMonth = new Date(new Date().setMonth(date.getMonth() - 1));
+  const nextMonth = new Date(new Date().setMonth(date.getMonth() + 1));
+  const prevDay = new Date(new Date().setDate(date.getDate() - 1));
+  const nextDay = new Date(new Date().setDate(date.getDate() + 1));
+  console.log(temp);
   return (
     <div>
-      Cities
-      <button type="button" onClick={() => setDate(new Date(date.setDate(date.getDate() - 1)))}>prev</button>
-      {a.map(({ date, id }) => <button key={id} type="button" onClick={() => setDate(date)}>{date.toISOString().substring(0, 10)}</button>)}
-      {date.toISOString().substring(0, 10)}
-      <button type="button" onClick={() => setDate(new Date(date.setDate(date.getDate() + 1)))}>next</button>
+      <header className="App-header">
+        {date.toISOString().substring(0, 10)}
+        {' '}
+        stats
+      </header>
+      <section className="banner">
+        <time className="date" dateTime={date.toISOString().substring(0, 10)}>
+          <div className="val">
+            <button type="button" onClick={() => setDate(prevYear)}>{prevYear.toISOString().substring(0, 4)}</button>
+            <span>
+              {date.toISOString().substring(0, 4)}
+            </span>
+            <button type="button" onClick={() => setDate(nextYear)}>{nextYear.toISOString().substring(0, 4)}</button>
+          </div>
+          <div className="val">
+            <button type="button" onClick={() => setDate(prevMonth)}>{prevMonth.toISOString().substring(5, 7)}</button>
+            <span>
+              {date.toISOString().substring(5, 7)}
+            </span>
+            <button type="button" onClick={() => setDate(nextMonth)}>{nextMonth.toISOString().substring(5, 7)}</button>
+          </div>
+          <div className="val">
+            <button type="button" onClick={() => setDate(prevDay)}>{prevDay.toISOString().substring(8, 10)}</button>
+            <span>
+              {date.toISOString().substring(8, 10)}
+            </span>
+            <button type="button" onClick={() => setDate(nextDay)}>{nextDay.toISOString().substring(8, 10)}</button>
+          </div>
+        </time>
+        <div>{total.today_new_confirmed}</div>
+        <button type="button" onClick={() => setDate(nextDay)}>
+          <img src="" alt="" />
+          next
+        </button>
+        <button type="button" onClick={() => setDate(prevDay)}>
+          <img src="" alt="" />
+          prev
+        </button>
+      </section>
       <ul>
         {countries.map((country) => (
           <li key={country.id}>
